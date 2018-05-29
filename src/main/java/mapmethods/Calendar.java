3196bf30b8dda83d56ad5d053407223bafc5e251
Simon Roberts <simon@dancingcloudservices.com>
2018-05-29 12:03:01 +0200

    Illustration of Calendar / Map merging, ordering, and setup for type-inferencing problem.

--- src/main/java/mapmethods/Calendar.java
+++ src/main/java/mapmethods/Calendar.java
@@ -3,8 +3,7 @@ package mapmethods;
 import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.temporal.TemporalAdjusters;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class Calendar {
     public static void main(String[] args) {
@@ -22,5 +21,20 @@ public class Calendar {
 
         home.forEach((k,v) -> System.out.println(k + " : " + v));
         work.forEach((k,v) -> System.out.println(k + " : " + v));
+
+        combined.putAll(home);
+//        home.forEach((k,v) -> combined.merge(k, v, (v1, v2) -> v1 + ", " + v2));
+        work.forEach((k,v) -> combined.merge(k, v, (v1, v2) -> v1 + ", " + v2));
+        System.out.println("Combined calendar");
+        combined.forEach((k,v) -> System.out.println(k + " : " + v));
+
+        System.out.println("or again...");
+        Comparator<Map.Entry<LocalDate, String>> cmelds = Comparator.comparing(e -> e.getKey()).reversed();
+//        cmelds = cmelds.reversed();
+
+        List<Map.Entry<LocalDate, String>> entries = new ArrayList<>(combined.entrySet());
+//        entries.sort((e1, e2) -> e1.getKey().compareTo(e2.getKey()));
+        entries.sort(cmelds);
+        entries.forEach(e -> System.out.println("On " + e.getKey() + " " + e.getValue()));
     }
 }
